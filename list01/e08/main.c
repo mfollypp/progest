907d341b12b96f4a0523d21fa9658c61233288b4
pedr0fontoura <pedrofontoura@id.uff.br>
2021-07-10 12:36:32 -0300

    Fix list 01 exercise 08

--- list01/e08/main.c
+++ list01/e08/main.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 
+int total_bills(int value);
+
 int main() {
 
   int value;
-  printf("Digite um valor inteiro em R$ para encontrar o menor número de notas necessárias: ");
+  printf("Digite um valor inteiro em R$ para encontrar o menor número de notas necessárias: R$");
   scanf("%d", &value);
 
+  int bills = total_bills(value);
+
+  printf("Total de notas: %d\n", bills);
+
+  return 0;
+}
+
+int total_bills(int value) {
   int bills_100 = 0, bills_50 = 0, bills_20 = 0, bills_10 = 0, bills_5 = 0, bills_2 = 0, bills_1 = 0;
 
   bills_100 = value / 100;
@@ -24,5 +34,5 @@ int main() {
   printf("%d x R$ 2\n", bills_2);
   printf("%d x R$ 1\n", bills_1);
 
-  return 0;
+  return bills_100 + bills_50 + bills_20 + bills_10 + bills_5 + bills_2 + bills_1;
 }
